848765d8c9168ac51c7a47eeca8acb22b7f2fd5c
mohanrajramalingam <mohanraj.r@gmail.com>
2016-03-05 14:46:35 -0800

    Changes from Web
    Changes from Web

--- src/main/java/com/apple/pages/LoginPage.java
+++ src/main/java/com/apple/pages/LoginPage.java
@@ -5,5 +5,6 @@ public class LoginPage {
 	public static void main(String[] args) {
 		System.out.println("This is test");
 		System.out.println("This is the line added from the branch - mraj");
+		System.out.println("This is the line added from the web");
 	}
 }
