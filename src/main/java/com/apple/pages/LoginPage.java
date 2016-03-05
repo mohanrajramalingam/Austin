640f64aa3fa53d243489b79f8a92023e8556562f
mohanrajramalingam <mohanraj.r@gmail.com>
2016-03-05 14:40:48 -0800

    Another commit from the branch

--- src/main/java/com/apple/pages/LoginPage.java
+++ src/main/java/com/apple/pages/LoginPage.java
@@ -4,5 +4,6 @@ public class LoginPage {
 
 	public static void main(String[] args) {
 		System.out.println("This is test");
+		System.out.println("This is the line added from the branch - mraj");
 	}
 }
