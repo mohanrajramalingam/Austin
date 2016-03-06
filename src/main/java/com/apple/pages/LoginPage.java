61ba41801768db0104e6c5d3636a7917ed04b5ed
mohanrajramalingam <mohanraj.r@gmail.com>
2016-03-06 14:35:26 -0800

    Changes to page object - 1

--- src/main/java/com/apple/pages/LoginPage.java
+++ src/main/java/com/apple/pages/LoginPage.java
@@ -1,15 +1,13 @@
 package com.apple.pages;
 
 public class LoginPage {
-
-	public static void main(String[] args) {
-		System.out.println("This is test");
-		System.out.println("This is the line added from the branch - mraj");
-		System.out.println("This is the line added from the web");
-		System.out.println("Changes to test the merge and upstream");
-		System.out.println("Changes to origin/master and see how it behaves");
-		System.out.println("Final changes for master to check the pull");
-		System.out.println("Final changes for mraj to check the pull");
-		System.out.println("Everything is perfect till this point");
-	}
+		int salary;
+		
+		public void setSalary(int salary) {
+			this.salary = salary;
+		}
+		
+		public int getSalary() {
+			return salary;
+		}
 }
