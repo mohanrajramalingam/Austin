7c50e2ba77525e173a63ee5e22a7c85dea2a8fc9
mohanrajramalingam <mohanraj.r@gmail.com>
2016-03-05 20:59:24 -0800

    Merge commit from the terminal

--- src/main/java/com/apple/pages/LoginPage.java
+++ src/main/java/com/apple/pages/LoginPage.java
@@ -6,5 +6,6 @@ public class LoginPage {
 		System.out.println("This is test");
 		System.out.println("This is the line added from the branch - mraj");
 		System.out.println("This is the line added from the web");
+		// Merge testing
 	}
 }
