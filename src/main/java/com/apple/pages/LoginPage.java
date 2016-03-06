cfca7381d8f6e46497fe4e2b8c8b31963422da50
mohanrajramalingam <mohanraj.r@gmail.com>
2016-03-06 10:23:49 -0800

    Changes - Everything is perfect till this point

--- src/main/java/com/apple/pages/LoginPage.java
+++ src/main/java/com/apple/pages/LoginPage.java
@@ -8,7 +8,8 @@ public class LoginPage {
 		System.out.println("This is the line added from the web");
 		System.out.println("Changes to test the merge and upstream");
 		System.out.println("Changes to origin/master and see how it behaves");
-		System.out.println("Final changes from master to check the pull");
+		System.out.println("Final changes for master to check the pull");
 		System.out.println("Final changes for mraj to check the pull");
+		System.out.println("Everything is perfect till this point");
 	}
 }
